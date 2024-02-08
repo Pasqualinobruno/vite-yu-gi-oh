257f310be0c145ea59fb33a5109fb4cde3671320
Pasqualibruno <pasqualinomolosso@gmail.com>
2024-02-08 17:21:06 +0100

    create element AppCard

---
+++ src/components/AppCard.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    name: 'AppCard',
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        card: Object
+    }
+}
+</script>
+
+<template>
+    <div class="col-12 col-sm-6 col-md-3 col-lg-5 col-xl-5">
+        <div class="card">
+            <img :src="card.card_images[0].image_url" alt="Card Image">
+            <div class="info_card">
+                <p class="name">{{ card.name }}</p>
+                <p class="archetype">{{ card.archetype }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    background-color: var(--yu-gi-oh-primary);
+
+    & .info_card {
+        padding: 1rem;
+        text-align: center;
+
+        & .name {
+            padding-bottom: 0.5rem;
+            color: var(--yu-gi-oh-light);
+            font-size: medium
+        }
+
+        & .archetype {
+            color: var(--yu-gi-oh-dark);
+        }
+    }
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,6 @@
 <script>
 import axios from 'axios';
+import AppCard from './AppCard.vue';
 export default {
     name: 'AppMain',
     data() {
@@ -7,8 +8,10 @@ export default {
             base_api_url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
             cards: null,
             error: false,
-
         }
+    },
+    components: {
+        AppCard,
     },
     mounted() {
         axios
@@ -37,18 +40,8 @@ export default {
 
             </select>
             <div class="number_cards">Found 39 cards</div>
-
-
             <div class="row">
-                <div class="col-12 col-sm-6 col-md-3 col-lg-5 col-xl-5" v-for="(card, index) in cards" :key="index">
-                    <div class="card">
-                        <img :src="card.card_images[0].image_url" alt="Card Image">
-                        <div class="info_card">
-                            <p class="name">{{ card.name }}</p>
-                            <p class="archetype">{{ card.archetype }}</p>
-                        </div>
-                    </div>
-                </div>
+                <AppCard v-for="(card, index) in cards" :key="index" :card="card"></AppCard>
             </div>
         </div>
     </main>
@@ -70,25 +63,6 @@ main {
             background-color: var(--yu-gi-oh-dark);
             padding: 0.5rem;
             color: var(--yu-gi-oh-light);
-        }
-
-        & .card {
-            background-color: var(--yu-gi-oh-primary);
-
-            & .info_card {
-                padding: 1rem;
-                text-align: center;
-
-                & .name {
-                    padding-bottom: 0.5rem;
-                    color: var(--yu-gi-oh-light);
-                    font-size: medium
-                }
-
-                & .archetype {
-                    color: var(--yu-gi-oh-dark);
-                }
-            }
         }
 
 
